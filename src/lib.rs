a1407bb721cf4dd3c134b021279d3c19f4961047
jakob <Jakobpstrobl@gmail.com>
2021-03-22 03:39:08 -0400

    Car explodes on give_boost event

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,36 @@
 use bakkesmod::prelude::*;
 use bakkesmod::wrappers::unreal::*;
-use bakkesmod::{game, console};
+use bakkesmod::game;
 
 #[plugin_init]
 pub fn on_load() {
-    // Hook event on_boost_pickup()
-    game::hook_event("Function TAGame.VehiclePickup_Boost_TA.Pickup", Box::new(on_boost_pickup));
+    // Hook event on_boost_pickup() only works on freeplay/LAN
+
+    // hook_event_with_caller() - callback is invoked when the event occurs 
+    // hook_event_with_caller_post() - callback is invoked after the event occurs (event is completed)
+    game::hook_event_with_caller_param("Function TAGame.VehiclePickup_Boost_TA.Pickup", Box::new(on_boost_pickup));
+    game::hook_event_with_caller("Function TAGame.CarComponent_Boost_TA.GiveBoost", Box::new(give_boost));
 }
 
-fn on_boost_pickup() {
+// The parameter for the callback is the object we hooked our event to 
+fn on_boost_pickup(boost: Box<BoostPickupWrapper>, param: Box<CarWrapper>) {
     log_console!("on_boost_pickup() invoked!");
+    // ADDRESS WAS tied to the boost we picked up
+    // Boost type: 
+    //   1 => Big boost
+    //   2 => Pads
+    log_console!("by: {} type {}", boost.addr(), boost.get_boost_type());
+
+    log_console!("param: {}", param.addr());
+} 
+
+fn give_boost(car: Box<CarComponentWrapper>) {
+    // address was tied to the car 
+    log_console!("give_boost() {}", car.addr());
+
+    // Destroys car if it exists
+    match car.get_car() {
+        Some(car) => car.demolish(),
+        None => log_console!("CarComponent does not contain car"),
+    }
 }
