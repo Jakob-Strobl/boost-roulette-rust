aee8ad39d9bc57f0ef9ce28e1afc4a98b6092506
jakob <Jakobpstrobl@gmail.com>
2021-03-22 23:22:30 -0400

    SDK now correctly returns car parameter for on_boost_pickup

--- src/lib.rs
+++ src/lib.rs
@@ -9,11 +9,10 @@ pub fn on_load() {
     // hook_event_with_caller() - callback is invoked when the event occurs 
     // hook_event_with_caller_post() - callback is invoked after the event occurs (event is completed)
     game::hook_event_with_caller_param("Function TAGame.VehiclePickup_Boost_TA.Pickup", Box::new(on_boost_pickup));
-    game::hook_event_with_caller("Function TAGame.CarComponent_Boost_TA.GiveBoost", Box::new(give_boost));
 }
 
 // The parameter for the callback is the object we hooked our event to 
-fn on_boost_pickup(boost: Box<BoostPickupWrapper>, param: Box<CarWrapper>) {
+fn on_boost_pickup(boost: Box<BoostPickupWrapper>, car: Box<CarWrapper>) {
     log_console!("on_boost_pickup() invoked!");
     // ADDRESS WAS tied to the boost we picked up
     // Boost type: 
@@ -21,28 +20,4 @@ fn on_boost_pickup(boost: Box<BoostPickupWrapper>, param: Box<CarWrapper>) {
     //   2 => Pads
     log_console!("by: {} type {}", boost.addr(), boost.get_boost_type());
     log_console!("param {}", param.addr());
-    param.demolish();
-
-    // log_console!("location of param {:?}", param.get_location());
-
-    // match param.get_car() {
-    //     Some(car) => {
-    //         log_console!("param: {}", car.addr());
-    //     },
-    //     None => log_console!("param does not contain car"),
-    // }
 } 
-
-fn give_boost(car: Box<CarComponentWrapper>) {
-    // address was tied to the car 
-    log_console!("give_boost() {}", car.addr());
-
-    // Destroys car if it exists
-    match car.get_car() {
-        Some(car) => {
-            log_console!("CarWrapper.addr = {}", car.addr());
-            // car.demolish();
-        },
-        None => log_console!("CarComponent does not contain car"),
-    }
-}
