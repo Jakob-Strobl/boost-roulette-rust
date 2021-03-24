dcf528b17956d51eca5a4b0c6912ba3aefd5f783
jakob <Jakobpstrobl@gmail.com>
2021-03-24 10:15:22 -0400

    Release 0.2
    The plugin is at MVP stage.
    
    - Chance to be demoed on boost pickup
    - Unique demo chance per boost type
    - Basic set file to enable or disable plugin
    
    Squashed commit of the following:
    
    commit 88dda0018ca2a7d7a99085a4eb3f86fb5c2e59f4
    Author: jakob <Jakobpstrobl@gmail.com>
    Date:   Wed Mar 24 10:09:54 2021 -0400
    
        BoostRoulette version 0.2
    
    commit ecb89549363b55adb47cf89cdccad78b6a12fb34
    Author: jakob <Jakobpstrobl@gmail.com>
    Date:   Wed Mar 24 10:09:22 2021 -0400
    
        Default chances use float division
    
    commit 5450ddbb028bbd95bfc5db7a9d3d38c9db7784eb
    Author: jakob <Jakobpstrobl@gmail.com>
    Date:   Wed Mar 24 10:05:56 2021 -0400
    
        Log on roll for testing
    
    commit 604ce7dd97d8986a1df94d282a3cd762d5a7ab5b
    Author: jakob <Jakobpstrobl@gmail.com>
    Date:   Wed Mar 24 09:55:42 2021 -0400
    
        Blow up based on chance per boost type
    
    commit 777deccf4c0ca0fc4f6388a11ac3f1750fb1198c
    Author: jakob <Jakobpstrobl@gmail.com>
    Date:   Wed Mar 24 09:46:49 2021 -0400
    
        Added dependency 'rand'
    
    commit 0bc9411287f0a97e32d50335e24aef99a843c42d
    Author: jakob <Jakobpstrobl@gmail.com>
    Date:   Wed Mar 24 08:32:58 2021 -0400
    
        Added set file for gui
    
    commit 33b9ee0e484596d0fd58b40297f4ba8b23665750
    Author: jakob <Jakobpstrobl@gmail.com>
    Date:   Wed Mar 24 08:24:53 2021 -0400
    
        Plugin can be enabled/disabled via cvar
    
    commit ab6c55f4ec90ba2b0b75d59d3b54036718b460a3
    Author: jakob <Jakobpstrobl@gmail.com>
    Date:   Wed Mar 24 08:01:05 2021 -0400
    
        Demo on pickup with demo animation!
    
    commit aee8ad39d9bc57f0ef9ce28e1afc4a98b6092506
    Author: jakob <Jakobpstrobl@gmail.com>
    Date:   Mon Mar 22 23:22:30 2021 -0400
    
        SDK now correctly returns car parameter for on_boost_pickup
    
    commit a40953de569dc6faa1edd3c79e93f79a3a55e891
    Author: jakob <Jakobpstrobl@gmail.com>
    Date:   Mon Mar 22 22:22:27 2021 -0400
    
        Car blows up in on_pickup event! I finally got the SDK to work
    
    commit a1407bb721cf4dd3c134b021279d3c19f4961047
    Author: jakob <Jakobpstrobl@gmail.com>
    Date:   Mon Mar 22 03:39:08 2021 -0400
    
        Car explodes on give_boost event

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,108 @@
+use std::process;
+use std::convert;
+
 use bakkesmod::prelude::*;
+use bakkesmod::prelude::cvar::CVar;
 use bakkesmod::wrappers::unreal::*;
-use bakkesmod::{game, console};
+use bakkesmod::game;
+use bakkesmod::console;
+
+use rand::prelude::*;
+
+// This is for better code clarity 
+// Boost type from BoostPickupWrapper.get_boost_type()
+//   1 => Big boost
+//   2 => Pads
+enum BoostType {
+    BIG,
+    PAD,
+}
+
+impl convert::From<u8> for BoostType {
+    fn from(val: u8) -> BoostType {
+        match val {
+            1 => BoostType::BIG,
+            2 => BoostType::PAD,
+            _ => process::abort(),
+        }
+    }
+}
+
+const NUM_PADS: usize = 28;
+const NUM_BIG_BOOSTS: usize = 6;
 
 #[plugin_init]
 pub fn on_load() {
-    // Hook event on_boost_pickup()
-    game::hook_event("Function TAGame.VehiclePickup_Boost_TA.Pickup", Box::new(on_boost_pickup));
+    // Enable button 
+    // TODO look into when and why you would want to save to cfg
+    let is_enabled = console::register_cvar("boost_roulette_is_enabled", "0", "Status of Boost Roulette Plugin (0 = Disabled | 1 = Enables)", true, true, 0.0, true, 1.0, false);
+    let big_boost_boom_chance = console::register_cvar("boost_roulette_big_boost_chance", &(100.0/NUM_BIG_BOOSTS as f32).to_string(), "The percent chance a big boost demos on pickup", true, true, 0.0, true, 100.0, false);
+    let pad_boom_chance = console::register_cvar("boost_roulette_pad_chance", &(100.0/NUM_PADS as f32).to_string(), "The percent chance a pad demos on pickup", true, true, 0.0, true, 100.0, false);
+    
+    console::add_on_value_changed(&is_enabled, Box::new(is_enabled_changed));
 }
 
-fn on_boost_pickup() {
-    log_console!("on_boost_pickup() invoked!");
+fn is_enabled_changed(_: String, is_enabled: CVar) {
+    log_console!("'is_enabled' => {}", is_enabled.get_bool_value());
+
+    if is_enabled.get_bool_value() {
+        // Hook event on_boost_pickup() only works on freeplay/LAN
+        // hook_event_with_caller() - callback is invoked when the event occurs 
+        // hook_event_with_caller_post() - callback is invoked after the event occurs (event is completed)
+        game::hook_event_with_caller_param("Function TAGame.VehiclePickup_Boost_TA.Pickup", Box::new(on_boost_pickup));
+    } else {
+        game::unhook_event("Function TAGame.VehiclePickup_Boost_TA.Pickup");
+    }
+}
+
+// The parameter for the callback is the object we hooked our event to 
+fn on_boost_pickup(boost: Box<BoostPickupWrapper>, car: Box<CarWrapper>) {
+    let boost_type = BoostType::from(boost.get_boost_type());
+
+    let is_unlucky = match boost_type {
+        BoostType::BIG => roll_big_boost(),
+        BoostType::PAD => roll_pad(),
+    };
+
+    if is_unlucky {
+        // Since CarWrapper implements RBactor (RBactor is a component of CarWrapper),
+        // we can just pass the adress of the CarWrapper as the RBactor 
+        car.demolish2(car.addr(), 0);
+    }
+} 
+
+fn get_roll() -> f32 {
+    thread_rng().gen_range(0.0 .. 100.0)
+}
+
+fn roll_big_boost() -> bool {
+    let roll = get_roll();
+    match console::get_cvar("boost_roulette_big_boost_chance") {
+        Some(chance) => {
+            let result = roll < chance.get_float_value();
+            log_console!("{} < {} = {}", roll, chance.get_float_value(), result);
+            
+            result
+        }
+        None => {
+            log_console!("'boost_roulette_big_boost_chance' cvar not found!");
+            false
+        },
+    }
+}
+
+fn roll_pad() -> bool {
+    let roll = get_roll();
+    match console::get_cvar("boost_roulette_pad_chance") {
+        Some(chance) => {
+            let result = roll < chance.get_float_value();
+            log_console!("{} < {} = {}", roll, chance.get_float_value(), result);
+            
+            result
+        }
+        None => {
+            log_console!("'boost_roulette_pad_chance' cvar not found!");
+            false
+        },
+    }
 }
