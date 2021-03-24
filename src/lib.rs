33b9ee0e484596d0fd58b40297f4ba8b23665750
jakob <Jakobpstrobl@gmail.com>
2021-03-24 08:24:53 -0400

    Plugin can be enabled/disabled via cvar

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,32 @@
 use std::vec;
 
 use bakkesmod::prelude::*;
+use bakkesmod::prelude::cvar::CVar;
 use bakkesmod::wrappers::unreal::*;
 use bakkesmod::game;
 use bakkesmod::console;
 
 #[plugin_init]
 pub fn on_load() {
-    // Hook event on_boost_pickup() only works on freeplay/LAN
-
-
-    // hook_event_with_caller() - callback is invoked when the event occurs 
-    // hook_event_with_caller_post() - callback is invoked after the event occurs (event is completed)
-    game::hook_event_with_caller_param("Function TAGame.VehiclePickup_Boost_TA.Pickup", Box::new(on_boost_pickup));
     // Enable button 
-    // console::register_cvar("demo_chance", default_value, description, searchable, has_min, min, has_max, max, save_to_cfg)
+    // TODO look into when and why you would want to save to cfg
+    let is_enabled = console::register_cvar("boost_roulette_is_enabled", "0", "Status of Boost Roulette Plugin (0 = Disabled | 1 = Enables)", true, true, 0.0, true, 1.0, false);
+    console::add_on_value_changed(&is_enabled, Box::new(is_enabled_changed));
+
+
 }
+
+fn is_enabled_changed(_: String, is_enabled: CVar) {
+    if is_enabled.get_bool_value() {
+        // Hook event on_boost_pickup() only works on freeplay/LAN
+        // hook_event_with_caller() - callback is invoked when the event occurs 
+        // hook_event_with_caller_post() - callback is invoked after the event occurs (event is completed)
+        game::hook_event_with_caller_param("Function TAGame.VehiclePickup_Boost_TA.Pickup", Box::new(on_boost_pickup));
+    } else {
+        game::unhook_event("Function TAGame.VehiclePickup_Boost_TA.Pickup");
+    }
+}
+
 
 // The parameter for the callback is the object we hooked our event to 
 fn on_boost_pickup(boost: Box<BoostPickupWrapper>, car: Box<CarWrapper>) {
