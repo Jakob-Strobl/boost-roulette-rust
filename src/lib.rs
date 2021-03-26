8e6ead9b8c837561fef4a9fc686bde2215084502
jakob <Jakobpstrobl@gmail.com>
2021-03-26 18:55:55 -0400

    Improved formatting

--- src/lib.rs
+++ src/lib.rs
@@ -34,10 +34,37 @@ const NUM_BIG_BOOSTS: usize = 6;
 #[plugin_init]
 pub fn on_load() {
     // Enable button 
-    // TODO look into when and why you would want to save to cfg
-    let is_enabled = console::register_cvar("boost_roulette_is_enabled", "0", "Status of Boost Roulette Plugin (0 = Disabled | 1 = Enables)", true, true, 0.0, true, 1.0, false);
-    let big_boost_boom_chance = console::register_cvar("boost_roulette_big_boost_chance", &(1.0/NUM_BIG_BOOSTS as f32).to_string(), "The probability a big boost demos on pickup", true, true, 0.0, true, 1.0, false);
-    let pad_boom_chance = console::register_cvar("boost_roulette_pad_chance", &(1.0/NUM_PADS as f32).to_string(), "The probability a pad demos on pickup", true, true, 0.0, true, 1.0, false);
+    // Save to cfg is like a way to save your settings? 
+    let is_enabled = console::register_cvar(
+        "boost_roulette_is_enabled", 
+        "0",
+        "Status of Boost Roulette Plugin (0 = Disabled | 1 = Enables)",
+        true,
+        true,
+        0.0,
+        true,
+        1.0,
+        true);
+    let big_boost_boom_chance = console::register_cvar(
+        "boost_roulette_big_boost_chance",
+        &(1.0/NUM_BIG_BOOSTS as f32).to_string(),
+        "The probability a big boost demos on pickup",
+        true,
+        true,
+        0.0,
+        true,
+        1.0,
+        true);
+    let pad_boom_chance = console::register_cvar(
+        "boost_roulette_pad_chance",
+        &(1.0/NUM_PADS as f32).to_string(),
+        "The probability a pad demos on pickup",
+        true,
+        true,
+        0.0,
+        true,
+        1.0,
+        true);
     
     console::add_on_value_changed(&is_enabled, Box::new(is_enabled_changed));
     console::add_on_value_changed(&big_boost_boom_chance, Box::new(boom_chance_changed));
